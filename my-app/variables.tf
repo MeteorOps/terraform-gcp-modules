d092cf596fbb0e88578239a47cff5d36f9245faa
arthur <arthur@arbemi.io>
2021-07-26 14:08:20 +0700

    added vpc and my-app (compute instance) modules

---
+++ my-app/variables.tf
@@ -0,0 +1,12 @@
+variable "project_id" {
+  description = "The project ID to host the network in"
+}
+
+variable "network_name" {
+}
+
+variable "network_subnet_name" {
+}
+
+variable "region" {
+}
---
+++ vpc/main.tf
@@ -0,0 +1,12 @@
+provider "null" {
+}
+
+module "vpc-module" {
+  source       = "terraform-google-modules/network/google"
+  version      = "~> 3.3.0"
+  project_id   = var.project_id
+  network_name = var.network_name
+  mtu          = 1460
+
+  subnets = var.subnets
+}
